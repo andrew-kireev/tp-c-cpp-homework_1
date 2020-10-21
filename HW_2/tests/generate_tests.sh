c19749e502a413936d5f469b443d09ac3d2ae9b9
Andrew <a-kireev1989@mail.ru>
2020-10-22 00:11:44 +0300

    Добавлены тесты

--- HW_2/tests/generate_tests.sh
+++ HW_2/tests/generate_tests.sh
@@ -3,3 +3,9 @@
 g++ generate_matrix.cpp -o generator
 ./generator size_10 10
 ./generator size_23 23
+./generator size_123 123
+./generator size_200 10
+./generator size_555 555
+./generator size_777 777
+./generator size_1333 1333
+./generator size_1777 1777
--- HW_2/tests/test_one_process.cpp
+++ HW_2/tests/test_one_process.cpp
@@ -2,10 +2,6 @@
 // Created by Andrew Kireev on 21.10.2020.
 //
 
-//
-// Created by Andrew Kireev on 18.10.2020.
-//
-
 #include <gtest/gtest.h>
 #include <string>
 #include <sys/mman.h>
@@ -75,73 +71,89 @@ TEST(read_file, test1_read_file) {
     free(matrix);
 }
 
-TEST(read_file, test2_read_file) {
+//TEST(read_file, test2_read_file) {
+//
+//    std::vector<int> test = {1, 2,
+//                             3, 4};
+//
+//    Matrix* matrix = read_file(SOURCE_DIR"tests/test2");
+//    bool is_equal = true;
+//    int size = matrix->size;
+//    int k = 0;
+//    for (int i = 0; i != size; ++i) {
+//        for (int j = 0; j != size; ++j) {
+//            if (test[k] != matrix->data[i*size+j]) {
+//                is_equal = false;
+//                break;
+//            }
+//            k++;
+//        }
+//    }
+//
+//    EXPECT_EQ(true, is_equal);
+//
+//    Calculation_res res = calculate_matrix(matrix);
+//    EXPECT_EQ(5, res.main_diagonal);
+//    EXPECT_EQ(5, res.side_diagonal);
+//    free(matrix->data);
+//    free(matrix);
+//}
 
-    std::vector<int> test = {1, 2,
-                             3, 4};
 
-    Matrix* matrix = read_file("/Users/andrewkireev/Documents/GitHub/tp-c-cpp-homework_1/HW_2/tests/test2");
-    bool is_equal = true;
-    int size = matrix->size;
-    int k = 0;
-    for (int i = 0; i != size; ++i) {
-        for (int j = 0; j != size; ++j) {
-            if (test[k] != matrix->data[i*size+j]) {
-                is_equal = false;
-                break;
-            }
-            k++;
-        }
-    }
+//TEST(multi_process, test_1_multi_process) {
+//
+//    char file_name[] = "/Users/andrewkireev/Documents/GitHub/tp-c-cpp-homework_1/HW_2/tests/5";
+//
+//    Calculation_res* res = multi_process(file_name, 5);
+//
+//    EXPECT_EQ(32, res->main_diagonal);
+//    EXPECT_EQ(14, res->side_diagonal);
+//    munmap(res, getpagesize());
+//}
+//
+//TEST(multi_process, test_4_multi_process) {
+//
+//    char file_name[] = "/Users/andrewkireev/Documents/GitHub/tp-c-cpp-homework_1/HW_2/tests/7";
+//
+//    Calculation_res* res = multi_process(file_name, 2);
+//
+//    EXPECT_EQ(24, res->main_diagonal);
+//    EXPECT_EQ(42, res->side_diagonal);
+//    munmap(res, getpagesize());
+//
+//    Calculation_res* res2 = multi_process(file_name, 2);
+//
+//    EXPECT_EQ(24, res2->main_diagonal);
+//    EXPECT_EQ(42, res2->side_diagonal);
+//    munmap(res2, getpagesize());
+//
+//    Calculation_res* res3 = multi_process(file_name, 3);
+//
+//    EXPECT_EQ(24, res3->main_diagonal);
+//    EXPECT_EQ(42, res3->side_diagonal);
+//    munmap(res3, getpagesize());
+//
+//    Calculation_res* res4 = multi_process(file_name, 50);
+//
+//    EXPECT_EQ(24, res4->main_diagonal);
+//    EXPECT_EQ(42, res4->side_diagonal);
+//    munmap(res3, getpagesize());
+//}
 
-    EXPECT_EQ(true, is_equal);
 
-    Calculation_res res = calculate_matrix(matrix);
-    EXPECT_EQ(5, res.main_diagonal);
-    EXPECT_EQ(5, res.side_diagonal);
-    free(matrix->data);
-    free(matrix);
-}
+TEST(multi_process, compare_algotithms_test1) {
 
+    char file_name[] = SOURCE_DIR"tests/size_10";
 
-TEST(multi_process, test_1_multi_process) {
+    Calculation_res* multi_proc_res = multi_process(file_name, 2);
 
-    char file_name[] = "/Users/andrewkireev/Documents/GitHub/tp-c-cpp-homework_1/HW_2/tests/5";
+    Matrix* matrix = read_file(SOURCE_DIR"tests/size_10");
 
-    Calculation_res* res = multi_process(file_name, 5);
+    Calculation_res one_proc_res = calculate_matrix(matrix);
 
-    EXPECT_EQ(32, res->main_diagonal);
-    EXPECT_EQ(14, res->side_diagonal);
-    munmap(res, getpagesize());
-}
-
-TEST(multi_process, test_4_multi_process) {
-
-    char file_name[] = "/Users/andrewkireev/Documents/GitHub/tp-c-cpp-homework_1/HW_2/tests/7";
-
-    Calculation_res* res = multi_process(file_name, 2);
-
-    EXPECT_EQ(24, res->main_diagonal);
-    EXPECT_EQ(42, res->side_diagonal);
-    munmap(res, getpagesize());
-
-    Calculation_res* res2 = multi_process(file_name, 2);
-
-    EXPECT_EQ(24, res2->main_diagonal);
-    EXPECT_EQ(42, res2->side_diagonal);
-    munmap(res2, getpagesize());
-
-    Calculation_res* res3 = multi_process(file_name, 3);
-
-    EXPECT_EQ(24, res3->main_diagonal);
-    EXPECT_EQ(42, res3->side_diagonal);
-    munmap(res3, getpagesize());
-
-    Calculation_res* res4 = multi_process(file_name, 50);
-
-    EXPECT_EQ(24, res4->main_diagonal);
-    EXPECT_EQ(42, res4->side_diagonal);
-    munmap(res3, getpagesize());
+    EXPECT_EQ(one_proc_res.main_diagonal, multi_proc_res->main_diagonal);
+    EXPECT_EQ(one_proc_res.side_diagonal, multi_proc_res->side_diagonal);
+    munmap(multi_proc_res, getpagesize());
 }
 
 
