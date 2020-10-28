838fa720997be96c87cc12bc16063b9ea29a38d3
Andrew <a-kireev1989@mail.ru>
2020-10-28 23:04:29 +0300

    Изменен мейн

--- HW_2/project/src/main.c
+++ HW_2/project/src/main.c
@@ -3,6 +3,7 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/mman.h>
 #include <unistd.h>
@@ -22,32 +23,37 @@ int main(int argc, char **argv) {
         if (scanf("%s", file_name) == 0)
             return 0;
 
+    Matrix* matrix;
+
+    matrix = read_file(file_name);
+    if (matrix == NULL)
+        return 0;
     struct  timeval start;
     gettimeofday(&start, NULL);
-    Calculation_res one_proc_res = calculate_matrix_one_proc(file_name);
+    Calculation_res* one_proc_res = calculate_matrix(matrix);
 
+    if (one_proc_res == NULL) {
+        free_matrix(matrix);
+        return 0;
+    }
     struct  timeval end;
     gettimeofday(&end, NULL);
     long time = (end.tv_sec - start.tv_sec) * 1000 + (end.tv_usec - start.tv_usec) / 1000;
 
     printf("Результат вычислений синхронного алгоритма:%c", '\n');
-    printf("Сумма главной диагонали = %d\n", one_proc_res.main_diagonal);
-    printf("Сумма побочной диагонали = %d\n", one_proc_res.side_diagonal);
+    printf("Сумма главной диагонали = %d\n", one_proc_res->main_diagonal);
+    printf("Сумма побочной диагонали = %d\n", one_proc_res->side_diagonal);
     printf("Время однопроцессорной версии: %ld миллисекунд\n", time);
 
-
-
-
-    int num_forks;
-    if (scanf("%d", &num_forks) != 1) {
-        num_forks = 10;
-    }
-
     gettimeofday(&start, NULL);
-    Calculation_multi_proc_res* multi_proc_res = multi_process(file_name, num_forks);
+    Calculation_res* multi_proc_res = multi_process(matrix);
     gettimeofday(&end, NULL);
-    if (multi_proc_res == NULL)
+
+    if (multi_proc_res == NULL) {
+        free(one_proc_res);
+        free_matrix(matrix);
         return 0;
+    }
     time = (end.tv_sec - start.tv_sec) * 1000 + (end.tv_usec - start.tv_usec) / 1000;
     printf("Результат вычислений много-процессорного алгоритма:%c", '\n');
     printf("Сумма главной диагонали = %d\n", multi_proc_res->main_diagonal);
@@ -55,12 +61,14 @@ int main(int argc, char **argv) {
     printf("Время многопроцессорной версии: %ld миллисекунд\n", time);
 
 
-    if (multi_proc_res->main_diagonal == one_proc_res.main_diagonal &&
-    multi_proc_res->side_diagonal == one_proc_res.side_diagonal)
+    if (multi_proc_res->main_diagonal == one_proc_res->main_diagonal &&
+    multi_proc_res->side_diagonal == one_proc_res->side_diagonal)
         printf("Результаты вычислений совпадают%c", '\n');
     else
         printf("Результаты не вычислений совпадают%c", '\n');
 
-    munmap(multi_proc_res, getpagesize());
+    free(one_proc_res);
+    free(multi_proc_res);
+    free_matrix(matrix);
     return 0;
 }
